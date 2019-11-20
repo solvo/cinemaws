af30155045cbd65b55269f060449532da9810411
Luis Zárate M <luis.zarate@solvosoft.com>
2019-11-20 01:28:49 -0600

    Vistas ModelViewSet y urls

--- mvtheater/generic_views.py
+++ mvtheater/generic_views.py
@@ -1,7 +1,14 @@
+from dateutil import parser
 from django.contrib.auth.models import User
+from django.utils import timezone
+from rest_framework import viewsets, filters
+from rest_framework.generics import ListAPIView
+
 
 from mvtheater.filters import User_filter
-from mvtheater.serializers import User_serializer
+from mvtheater.models import Clients, Movies, Cinemas, Matinees, Tickets
+from mvtheater.serializers import User_serializer, Clients_serializer, Movies_serializer, Cinemas_serializer, \
+    Matinees_serializer, Tickets_serializer
 from mvtheater.views import My_view
 
 
@@ -13,3 +20,64 @@ class User_list(My_view):
     def get_queryset(self, request):
         queryset = self.queryset.filter(id=request.user.id)
         return queryset
+
+class Users_rest_view(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = User_serializer
+    filterset_class = User_filter
+
+
+
+class Clients_rest_view(viewsets.ModelViewSet):
+    queryset = Clients.objects.all()
+    serializer_class = Clients_serializer
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['user__username', ]
+
+
+
+class Movies_rest_view(viewsets.ModelViewSet):
+    queryset = Movies.objects.all()
+    serializer_class = Movies_serializer
+    filter_backends = [filters.OrderingFilter]
+    ordering_fields = ['name', ]
+
+
+
+class Cinemas_rest_view(viewsets.ModelViewSet):
+    queryset = Cinemas.objects.all()
+    serializer_class = Cinemas_serializer
+
+
+class Matinees_rest_view(viewsets.ModelViewSet):
+    queryset = Matinees.objects.all()
+    serializer_class = Matinees_serializer
+
+
+
+class Tickets_rest_view(viewsets.ModelViewSet):
+    queryset = Tickets.objects.all()
+    serializer_class = Tickets_serializer
+
+class Billboard_rest_view(ListAPIView, viewsets.ViewSet):
+    lookup_url_kwarg = "date"
+    serializer_class = Matinees_serializer
+    queryset = Matinees.objects.all()
+    #renderer_classes = [JSONRenderer]
+    filterset_fields = ['data_time']
+
+    def get_queryset(self):
+
+        querydate = timezone.now().date()
+        uoption = self.request.query_params.get('date')
+        if uoption != None:
+            try:
+                newdate = parser.parse(uoption)
+                if newdate:
+                    querydate=newdate.date()
+            except ValueError as e:
+                pass
+            except OverflowError as eo:
+                pass
+
+        return Matinees.objects.filter(data_time__date=querydate)
--- mvtheater/urls.py
+++ mvtheater/urls.py
@@ -1,12 +1,29 @@
-from django.urls import path, re_path
+from django.urls import path, include, re_path
+from rest_framework import routers
 
-from mvtheater.generic_views import User_list
-from mvtheater.views import  annotate_view, aggregate_view
+from mvtheater.generic_views import Clients_rest_view, Movies_rest_view, Cinemas_rest_view, \
+    Matinees_rest_view, Tickets_rest_view, Billboard_rest_view, User_list
+from mvtheater.views import Moviedetails_rest_view, annotate_view, aggregate_view
+from mvtheater.views import Rest_get_object
+
+router = routers.DefaultRouter()
+router.register('clients', Clients_rest_view)
+router.register('movies', Movies_rest_view)
+router.register('cinemas', Cinemas_rest_view)
+router.register('matinees', Matinees_rest_view)
+router.register('tickets', Tickets_rest_view)
+router.register('billboard', Billboard_rest_view)
 
 urlpatterns = [
+    path('rest/', include(router.urls)),
     path('rest/users/', User_list.as_view()),
+    path('rest/movies/details/<str:pk>/', Moviedetails_rest_view.as_view()),
+    path('rest/test/<int:pk>/', Rest_get_object.as_view()),
+    path('rest/movies/details/<str:pk>/', Moviedetails_rest_view.as_view(), name='movdet'),
     path('rest/annotate/', annotate_view, name='annotate'),
     re_path(r'rest/aggregate/(?P<datein>\d{4}-\d{2}-\d{2})/(?P<dateend>\d{4}-\d{2}-\d{2})$', aggregate_view, name='aggregate'),
 ]
+
+
 
 
