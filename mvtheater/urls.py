e752450ed08a1388b53905a159d784afc1b3843a
Luis Zárate M <luis.zarate@solvosoft.com>
2019-11-20 02:23:41 -0600

    segundo aggregate

--- mvtheater/urls.py
+++ mvtheater/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path, re_path
 
 
-from mvtheater.views import  annotate_view, aggregate_view
+from mvtheater.views import annotate_view, aggregate_view, aggregate_view2
 
 urlpatterns = [
     path('rest/annotate/', annotate_view, name='annotate'),
     re_path(r'rest/aggregate/(?P<datein>\d{4}-\d{2}-\d{2})/(?P<dateend>\d{4}-\d{2}-\d{2})$', aggregate_view, name='aggregate'),
+    re_path(r'rest/aggregate2/(?P<datein>\d{4}-\d{2}-\d{2})/(?P<dateend>\d{4}-\d{2}-\d{2})$', aggregate_view2),
 ]
 
 
